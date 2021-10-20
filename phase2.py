ac5ce081e5d95d68be27772e647ea23467ff0012
Neil Franks <neilmichaelfranks@gmail.com>
2021-10-20 13:38:17 -0700

    training patient

--- phase2.py
+++ phase2.py
@@ -1,30 +1,30 @@
 from pipelines import pipeline
-# from run_qg import run_qg
+from run_qg import run_qg
 
-# args_dict = {
-#     "model_name_or_path": "valhalla/t5-small-e2e-qg",
-#     "model_type": "t5",
-#     "tokenizer_name_or_path": "t5_qg_tokenizer",
-#     "output_dir": "t5-doctor",
-#     "train_file_path": "data\doctor\doctor_train_data_e2e_qg_t5.pt",
-#     "valid_file_path": "data\doctor\doctor_valid_data_e2e_qg_t5.pt",
-#     "per_device_train_batch_size": 32,
-#     "per_device_eval_batch_size": 32,
-#     "gradient_accumulation_steps": 8,
-#     "learning_rate": 1e-4,
-#     # "learning_rate": 0.0,
-#     "num_train_epochs": 900,
-#     "seed": 42,
-#     "do_train": True,
-#     "do_eval": True,
-#     "evaluate_during_training": True,
-#     "logging_steps": 100
-# }
-# # start training
-# run_qg(args_dict)
+args_dict = {
+    "model_name_or_path": "valhalla/t5-small-e2e-qg",
+    "model_type": "t5",
+    "tokenizer_name_or_path": "t5_qg_tokenizer",
+    "output_dir": "t5-patient",
+    "train_file_path": "data\patient\patient_train_data_e2e_qg_t5.pt",
+    "valid_file_path": "data\patient\patient_valid_data_e2e_qg_t5.pt",
+    "per_device_train_batch_size": 32,
+    "per_device_eval_batch_size": 32,
+    "gradient_accumulation_steps": 8,
+    "learning_rate": 1e-4,
+    "num_train_epochs": 900,
+    "seed": 42,
+    "do_train": True,
+    "do_eval": True,
+    "evaluate_during_training": True,
+    "logging_steps": 100
+}
+# start training
+run_qg(args_dict)
 
-nlp = pipeline("e2e-qg", model="t5-doctor")
-print(nlp("heat"))
+nlp = pipeline("e2e-qg", model="t5-patient")
+print("Symptom: %s\nQuestion: %s" %
+      ("Gtfo get me a new doctor", nlp("anorexia")))
 print(nlp("hemorrhoid"))
 print(nlp("blood in stool"))
 print(nlp("abdominal pain"))
@@ -32,3 +32,4 @@ print(nlp("diarrhea"))
 print(nlp("hiccup,counter-current"))
 print(nlp("restlessness"))
 print(nlp("helicobacter infection"))
+print(nlp("heat"))
